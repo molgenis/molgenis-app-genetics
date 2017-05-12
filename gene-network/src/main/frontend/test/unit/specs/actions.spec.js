0efe72c1f56ca85a1b699b42bacbd39bdb83b040
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-12 14:17:50 +0200

    Move testAction helper method to separate utils script

--- gene-network/src/main/frontend/test/unit/specs/actions.spec.js
+++ gene-network/src/main/frontend/test/unit/specs/actions.spec.js
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import testAction from '../utils/action.utils'
 import td from 'testdouble'
 
 import {
@@ -385,51 +385,3 @@ describe('Testing actions', () => {
     })
   })
 })
-
-// helper for testing action with expected mutations
-const testAction = (action, payload, state, expectedMutations, expectedActions, done) => {
-  let mutationCount = 0
-  let actionCount = 0
-
-  const commit = (type, payload) => {
-    const mutation = expectedMutations[mutationCount]
-    try {
-      expect(mutation.type).to.equal(type)
-      if (payload) {
-        expect(mutation.payload).to.deep.equal(payload)
-      }
-    } catch (error) {
-      done(error)
-    }
-
-    mutationCount++
-    if (mutationCount >= expectedMutations.length) {
-      done()
-    }
-  }
-
-  const dispatch = (type, payload) => {
-    const action = expectedActions[actionCount]
-    try {
-      expect(action.type).to.equal(type)
-      if (payload) {
-        expect(action.payload).to.deep.equal(payload)
-      }
-    } catch (error) {
-      done(error)
-    }
-
-    actionCount++
-    if (mutationCount >= expectedMutations.length) {
-      done()
-    }
-  }
-
-  action({commit, dispatch, state}, payload)
-
-  if (expectedMutations.length === 0 && expectedActions.length === 0) {
-    expect(mutationCount).to.equal(0)
-    expect(actionCount).to.equal(0)
-    done()
-  }
-}
