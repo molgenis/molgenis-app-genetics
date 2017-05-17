d7e0d1c84e3fbfbc7b2ad406ee632abdce313daa
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-17 11:36:39 +0200

    Add download button to table

--- gene-network/src/main/frontend/src/components/VariantTable.vue
+++ gene-network/src/main/frontend/src/components/VariantTable.vue
@@ -16,21 +16,47 @@
 
 
     <b-table hover :items="variants" :fields="fields" :filter="filter"></b-table>
+    <a id="download-anchor" style="display:none"></a>
   </div>
 </template>
-
 <style>
   .table-search-field {
     float: left
   }
+
   .table-download-button {
     float: right
   }
 </style>
 <script>
+  import csv from 'to-csv'
+
   export default {
     name: 'variant-table',
     props: ['variants'],
+    methods: {
+      download: function () {
+        const data = []
+        this.variants.map(function (variant) {
+          data.push({
+            '#CHROM': variant['#CHROM'],
+            'Gene_Name': variant['Gene_Name'],
+            'cDNA': variant['cDNA'],
+            'pChange': variant['pChange'],
+            'gavinReason': variant['gavinReason']
+          })
+        })
+        const result = (csv(data))
+
+        console.log('data:text/csv;charset=utf-8;base64,' + btoa(result))
+
+        // HTML workaround because all available file system packages on npm are broken
+        const dlAnchorElem = document.getElementById('download-anchor')
+        dlAnchorElem.setAttribute('href', 'data:text/csv;charset=utf-8;base64,' + btoa(result.trim()))
+        dlAnchorElem.setAttribute('download', 'patient_' + this.$route.params.entityTypeId)
+        dlAnchorElem.click()
+      }
+    },
     data: function () {
       return {
         fields: {
