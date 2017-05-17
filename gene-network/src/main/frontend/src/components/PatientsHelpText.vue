2ce114f032876b8e817242c0c1bac2ee7630916c
Mark-de-Haan <markdehaan90@gmail.com>
2017-05-17 09:33:52 +0200

    Update patient help text

--- gene-network/src/main/frontend/src/components/PatientsHelpText.vue
+++ gene-network/src/main/frontend/src/components/PatientsHelpText.vue
@@ -2,7 +2,12 @@
   <div class="row">
     <div class="col-md-12">
       <h4>Patients</h4>
-      <p>Select a patient from the menu to view its details</p>
+      <hr>
+      View variants for each patient (VCF) by clicking on a patient link on the left.
+      <br><br>
+      When viewing a patient, you can look for HPO phenotypes. Phenotype selection will sort variants based on GeneNetwork score.
+      <br><br>
+      To import a patient (VCF), you can either go to the <a href="/menu/main/diagnostics">Diagnostic annotator</a>, or import a VCF that was already annotated with GAVIN <a href="/upload">here</a>
     </div>
   </div>
 </template>
