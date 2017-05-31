2d093d64a61e5fc3bfcbe9026ea081142b757004
Bart Charbon <b.charbon@umcg.nl>
2017-05-31 09:54:16 +0200

    Fix unit test

--- gene-network/src/main/frontend/test/unit/specs/components/VariantTable.spec.js
+++ gene-network/src/main/frontend/test/unit/specs/components/VariantTable.spec.js
@@ -59,7 +59,7 @@ describe('VariantTable.vue', () => {
       }
     ]
     const actualData = getRenderedText(VariantTable, {variants: propsData})
-    const expectedData = '<div><table class="table table-hover"><thead><tr><th class="">Chromosome</th><th class="">Gene</th><th class="">cDNA</th><th class="">Protein change</th><th class="">Gavin Reason</th></tr></thead><tbody><tr class=""><td class="">1</td><td class="">BRCA1</td><td class="">A&gt;T</td><td class="">Arg&gt;His</td><td class="">Pathogenic because test</td></tr><tr class=""><td class="">2</td><td class="">BRCA2</td><td class="">C&gt;G</td><td class="">Lys&gt;Ala</td><td class="">Benign because test</td></tr></tbody></table> <div class="table-download-button"><button class="table-download-button btn btn-info btn-md">Download all variants</button> <br><br></div> <a id="download-anchor" style="display: none;"></a></div>'
+    const expectedData = '<div><table class="table table-hover"><thead><tr><th class="">Chromosome</th><th class="">Gene</th><th class="">cDNA</th><th class="">Protein change</th><th class="">Gavin Reason</th></tr></thead><tbody><tr class=""><td class="">1</td><td class="">BRCA1</td><td class="">A&gt;T</td><td class="">Arg&gt;His</td><td class="">Pathogenic because test</td></tr><tr class=""><td class="">2</td><td class="">BRCA2</td><td class="">C&gt;G</td><td class="">Lys&gt;Ala</td><td class="">Benign because test</td></tr></tbody></table> <div class="table-download-button"><button class="table-download-button btn btn-info btn-md">Download</button> <br><br></div> <a id="download-anchor" style="display: none;"></a></div>'
 
     expect(actualData).to.equal(expectedData)
   })
